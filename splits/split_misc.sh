039250205b54f9448edac4c5495ea8ce910cefef
Chesnay <chesnay@apache.org>
2018-08-01 15:44:53 +0200

    Add TTL tests

--- splits/split_misc.sh
+++ splits/split_misc.sh
@@ -58,5 +58,8 @@ run_test "Elasticsearch (v5.1.2) sink end-to-end test" "$END_TO_END_DIR/test-scr
 run_test "Quickstarts Java nightly end-to-end test" "$END_TO_END_DIR/test-scripts/test_quickstarts.sh java"
 run_test "Quickstarts Scala nightly end-to-end test" "$END_TO_END_DIR/test-scripts/test_quickstarts.sh scala"
 
+run_test "State TTL Heap backend end-to-end test" "$END_TO_END_DIR/test-scripts/test_stream_state_ttl.sh file"
+run_test "State TTL RocksDb backend end-to-end test" "$END_TO_END_DIR/test-scripts/test_stream_state_ttl.sh rocks"
+
 printf "\n[PASS] All tests passed\n"
 exit 0
